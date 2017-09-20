651b5aab93d5360946e026a73ae7376034de47d6
sujianglong <409722281@qq.com>
2017-09-20 11:59:39 +0800

    更改配置文件

--- www/ImageEdit.js
+++ www/ImageEdit.js
@@ -1,6 +1,7 @@
 var exec = require("cordova/exec");
 var ImageEdit = {};
 ImageEdit.imageprocess = function (successCallback, errorCallback, args) {
+    // var args = ["path"]
     exec(successCallback, errorCallback, "ImageProcess", "openCamera", args);
 };
 module.exports = ImageEdit;
