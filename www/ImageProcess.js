0cb01b5734ad942efa36023ccfb54a1ec39ea5f5
hetong <455475931@qq.com>
2017-09-27 09:53:43 +0800

    修改导出方式

--- www/ImageProcess.js
+++ www/ImageProcess.js
@@ -1,22 +1,14 @@
 var exec = require('cordova/exec');
-// var ImageProcess = {
-// openCamera: function (savedFilePath, success, error) (
-//                                                       exec(success, error, "ImageProcess", "openCamera", [savedFilePath])
-//                                                       }),
-// openAlbum: function (savedFilePath, success, error) (
-//                                                      exec(success, error, "ImageProcess", "openAlbum", [savedFilePath])
-//                                                      }),
-// openCrop: function (savedFilePath, success, error) (
-//                                                     exec(success, error, "ImageProcess", "openCrop", [savedFilePath,selectedFilePath])
-//                                                     }),
-// }
-// exports.exports = ImageProcess;
-exports.openCamera = function (savedFilePath, success, error) {
-    exec(success, error, "ImageProcess", "openCamera", [savedFilePath]);
+var ImageProcess = {
+    openCamera: function (savedFilePath, success, error) {
+        exec(success, error, "ImageProcess", "openCamera", [savedFilePath]);
+    },
+    openAlbum: function (savedFilePath, success, error){
+        exec(success, error, "ImageProcess", "openAlbum", [savedFilePath]);
+    },
+    openCrop: function (savedFilePath, success, error){
+        exec(success, error, "ImageProcess", "openCrop", [savedFilePath,selectedFilePath])
+    }
 };
-exports.openAlbum = function (savedFilePath, success, error) {
-    exec(success, error, "ImageProcess", "openAlbum", [savedFilePath]);
-};
-exports.openCrop = function (savedFilePath,selectedFilePath, success, error) {
-    exec(success, error, "ImageProcess", "openCrop", [savedFilePath,selectedFilePath]);
-};
+
+module.exports = ImageProcess;
