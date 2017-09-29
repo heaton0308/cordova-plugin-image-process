7961ab0dd10c319762342083683ce4ee1229476a
Byf <13488410875@163.com>
2017-09-29 10:41:55 +0800

    修改输入id

--- src/ios/MyCamera/MyCameraCropper/ClipViewController.m
+++ src/ios/MyCamera/MyCameraCropper/ClipViewController.m
@@ -276,7 +276,7 @@
 {
     // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了
     UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
-    
+
     [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
 
     UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
